7f669e5978c1c07a1635a72956281fcd1a196eb2
vasiliyisaev <vasyaisaev31@gmail.com>
2020-04-21 20:14:27 +0300

    init

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def handleJson(filename="BGC0001763_antiSMASH_5.0.0/BGC0001763.json"):
     with open(filename, 'r') as f:
         data = json.load(f)
     for item in data["records"][0]["modules"]["antismash.modules.nrps_pks"]["domain_predictions"].keys():
-        if item.startswith("nrpspksdomains_ctg1_19_AMP-binding"):
+        if "AMP-binding" in item:
             ret_data.append(data["records"][0]["modules"]["antismash.modules.nrps_pks"]["domain_predictions"][item][
                                 "NRPSPredictor2"]["stachelhaus_seq"].upper())
             ret_names_formated = item.split("_")
@@ -81,6 +81,7 @@ def handleJson(filename="BGC0001763_antiSMASH_5.0.0/BGC0001763.json"):
                                 ret_names_formated[2] +
                                 "_A" + ret_names_formated[3].split(".")[-1])
             ret_names.append(ret_names_tmp)  # nrpspksdomains_ctg1_19_AMP-binding.7
+    print(ret_names)
     return ret_data, ret_names
 
 
@@ -118,25 +119,32 @@ def export_result(data, title, filename="testSource.csv"):
             i += 1
 
 
+# Strinct false - продлеваем, пока скор равен (скору на позиции N +- eps)
 def calc_metric_nmax(res, max_n=5, strict=True):
     test_data = load_test_data()
     score = 0
     cur = 0
     test_res = []
+    wrong_description = 0
     for item in res:
         test_status = False
         names = list(item.keys())
         if strict:
             for i in range(max_n):
-                # print("{} <> {} --> {}".format(names[i].lower(), test_data[cur].split('\t')[-1].lower(), names[i].lower() == test_data[cur].split('\t')[-1].lower()))
-                if names[i].lower() == test_data[cur].split('\t')[-1].lower():
+                #print("{} <> {} --> {}".format(names[i].lower(), test_data[cur].split('\t')[-1].lower(), names[i].lower() == test_data[cur].split('\t')[-1].lower()))
+                formated_data = test_data[cur].split('\t')[-1].split("-")[-1].lower()
+                if (formated_data is None or not len(formated_data)== 3):
+                    wrong_description += 1
+                    break
+
+                elif names[i].lower() == formated_data:
                     score += 1
                     test_status = True
                     break
         else:
-            best_score = list(item.values())[0]
+            best_score = list(item.values())[max_n]
             i = 0
-            while list(item.values())[i] == best_score and i < max_n:
+            while list(item.values())[i] >= best_score and i < len(names):
                 if names[i].lower() == test_data[cur].split('\t')[-1].lower():
                     score += 1
                     test_status = True
@@ -144,12 +152,16 @@ def calc_metric_nmax(res, max_n=5, strict=True):
                 i += 1
         test_res.append(int(test_status))
         cur += 1
-    return score / len(test_data), test_res
+    print(wrong_description / len(test_data) * 100 )
+    return score / (len(test_data) - wrong_description), test_res
 
 
 def toFixed(numObj, digits=0):
     return f"{numObj:.{digits}f}"
 
+
+import string
+import random
 
 def testMain():
     test_data = load_test_data()
@@ -166,28 +178,49 @@ def testMain():
 
         tmp_export = ""
         for prec in tmp_res.items():
-            tmp_export += "{}:({});".format(prec[0], toFixed((prec[1] + 1) * 10, 1))  # TODO: format
+            tmp_export += "{}:({});".format(prec[0], toFixed((prec[1] + 1) * 10, 1))
         allResults.append(tmp_export)
 
     for item in res:
         names = list(item.keys())
         singleResults.append(names[0])
 
-    score_top1, top1_res = calc_metric_nmax(res, 1, False)
-    score_top3, top3_res = calc_metric_nmax(res, 3, False)
-    score_top5, top5_res = calc_metric_nmax(res, 5, False)
+    # TODO: fix my NAN-values
+    fixed = False
+    if fixed:
+        ref_res = []
+        for item in test_data:
+            tmp_ref_res = {}
+            for tmp_item in item.split("\t")[2].split(";"):
+                tmp_ref_res[tmp_item.split("(")[0]] = float(tmp_item[-5:-1])
+            ref_res.append(tmp_ref_res)
+
+        ref_score_top1, ref_top1_res = calc_metric_nmax(ref_res, 1)
+        ref_score_top3, ref_top3_res = calc_metric_nmax(ref_res, 3)
+        ref_score_top5, ref_top5_res = calc_metric_nmax(ref_res, 5)
+
+    score_top1, top1_res = calc_metric_nmax(res, 1)
+    score_top3, top3_res = calc_metric_nmax(res, 3)
+    score_top5, top5_res = calc_metric_nmax(res, 5)
 
     ref_score = 0
-
     for item in test_data:
-        print(item)
         if item.split("\t")[2][:3].lower() == item.split("\t")[-1].lower():
             ref_score += 1
 
-    export_result([allResults, singleResults, top1_res, top3_res, top5_res], ["MY_FULL_PRECISION", "MY_SINGEL_PRECISION", "top1", "top3", "top5"])
+    aa_id = []
+    ref_single_res = []
+    for item in test_data:
+        aa_id.append(item.split("\t")[-1].lower())
+        ref_single_res.append(item.split("\t")[2][:3].lower())
+
+    export_result([aa_id, ref_single_res, allResults, singleResults, top1_res, top3_res, top5_res], ["aa_id", "NRPsPred2_single", "MY_FULL_PRECISION", "MY_SINGEL_PRECISION", "top1", "top3", "top5"])
     print("score top1 = ", score_top1)
     print("score top3 = ", score_top3)
     print("score top5 = ", score_top5)
+    #print("ref score top1 = ", ref_score_top1)
+    #print("ref score top3 = ", ref_score_top3)
+    #print("ref score top5 = ", ref_score_top5)
     print("ref_score = ", ref_score / len(test_data))
 
 
